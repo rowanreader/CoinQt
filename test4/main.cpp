14729be35f28aaaf09e22f1be2daeda97d28c1a2
Jacqueline Heaton <16jh12@queensu.ca>
2019-05-14 09:51:28 -0400

    sphere

--- test4/main.cpp
+++ test4/main.cpp
@@ -9,25 +9,31 @@
 #include <Quarter/Quarter.h>
 #include <Quarter/QuarterWidget.h>
 #include <Quarter/eventhandlers/DragDropHandler.h>
-#include "ui_QuarterViewer.h"
+#include <ui_QuarterViewer.h>
 
 using namespace SIM::Coin3D::Quarter;
 int main(int argc, char *argv[])
 {
+	// start application
 	QApplication a(argc, argv);
 	Quarter::init();
+
+	// widget = screen we see
 	QWidget * viewer = new QWidget;
+	// style?
 	Ui::QuarterViewer ui;
 	ui.setupUi(viewer);
+
 	SoSeparator * root = new SoSeparator;
 	root->ref();
+	// new thing/material
 	SoMaterial* m = new SoMaterial();
-	m->diffuseColor.setValue(1, 0, 0);
-	root->addChild(m);
-	root->addChild(new SoSphere);
-	//QuarterWidget * viewer = new QuarterWidget;
-	ui.QuarterWidget->setSceneGraph(root);
-	//viewer->setSceneGraph(root);
-	viewer->show();
+	m->diffuseColor.setValue(1, 0, 0); // make red
+	root->addChild(m); // add to root
+	root->addChild(new SoSphere); // add sphere to root
+
+	ui.QuarterWidget->setSceneGraph(root); // format??
+
+	viewer->show(); // show widget
 	return a.exec();
 }
